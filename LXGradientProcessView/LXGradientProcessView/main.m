c96af598034dfd9cc06e6327291cfce1894c9eed
xl20071926 <lixin_1990@yeah.net>
2017-04-21 11:33:40 +0800

    add files

---
+++ LXGradientProcessView/LXGradientProcessView/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  LXGradientProcessView
+//
+//  Created by Leexin on 15/12/20.
+//  Copyright © 2015年 Garden.Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
